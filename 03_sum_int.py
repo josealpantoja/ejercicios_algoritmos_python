14a5d7000fea24e24474538695a72cc77a9d779b
Joseal Pantoja Durán <joseal.pantoja@gmail.com>
2023-08-23 01:12:21 -0600

    actualizamos segundo algoritmo de ejercicio 3

--- 03_sum_int.py
+++ 03_sum_int.py
@@ -19,3 +19,27 @@ print('*' * 20)
 print('')
 
 #Ejericcio 2
+
+print('')
+print('🚨 Desarrolle un algoritmo que realice la sumatoria de los números enteros múltiplos de 5, comprendidos entre el 1 y el 100, es decir, 5 + 10 + 15 +.... + 100. El programa deberá imprimir los números en cuestión y finalmente su sumatoria')
+print('')
+print('*' * 20)
+print('')
+
+multiple = 5
+i = 1
+sum = 0
+
+while multiple < 100:
+
+    multiple = i * 5
+    i += 1
+    sum = sum + multiple
+    print(multiple)
+
+
+print('La suma de los multimos del 5 entre el 1 y el 100 es 👉 ', sum)
+
+print('')
+print('*' * 20)
+print('')
