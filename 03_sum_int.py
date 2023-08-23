6a0876ea0d76f9d82cd7b9fdaca87aee3018e4ea
Joseal Pantoja Durán <joseal.pantoja@gmail.com>
2023-08-23 00:30:23 -0600

    agregamos ejercicio 3, sumatoria de numero enteros

---
+++ 03_sum_int.py
@@ -0,0 +1,21 @@
+#Ejercicio 1
+print('')
+print('🚨 Desarrolle un algoritmo que realice la sumatoria de los números enteros comprendidos entre el 1 y el 10, es decir, 1 + 2 + 3 + .... + 10.')
+print('')
+print('*' * 20)
+print('')
+
+
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+n = 0
+
+for i in numbers:
+    n = n + i
+
+print('El resultado de la sumatoria de', numbers, 'es', n)
+
+print('')
+print('*' * 20)
+print('')
+
+#Ejericcio 2
